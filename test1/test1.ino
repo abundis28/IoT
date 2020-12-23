0dee856d1a13ffcc0379d6eff2fe020b28477b43
Andrés <andresabundis@gmail.com>
2020-12-23 12:53:10 -0600

    Improves style and pin definition.

--- test1/test1.ino
+++ test1/test1.ino
@@ -1,12 +1,20 @@
+///////////////////////////////
+//  Constant definition
+
+const int led_Pin = 8;
+
+///////////////////////////////
+//  Code
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(8, OUTPUT);
+  pinMode(led_Pin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(8, HIGH);  // Turn led on
+  digitalWrite(led_Pin, HIGH);  // Turn led on
   delay(500);             // Wait for half second  
-  digitalWrite(8, LOW);   // Turn led off
+  digitalWrite(led_Pin, LOW);   // Turn led off
   delay(500);             // Wait for half second
 }
