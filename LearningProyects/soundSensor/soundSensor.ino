76c21e621c73167f7c51157b2fe7767f600e29c7
Andrés <andresabundis@gmail.com>
2020-12-29 19:35:45 -0600

    Creates file for eventual sound sensor practice project.

---
+++ LearningProyects/soundSensor/soundSensor.ino
@@ -0,0 +1,17 @@
+///////////////////////////////
+//  Definitions
+
+const int led_Pin = 8;
+
+///////////////////////////////
+//  Code
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
