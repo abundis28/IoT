6b136ec36b5813e587806f709367b5bd72e48d61
Andrés <andresabundis@gmail.com>
2021-01-19 19:29:36 -0600

    Adds code for digital output sound sensor.

--- LearningProyects/soundSensor/soundSensor.ino
+++ LearningProyects/soundSensor/soundSensor.ino
@@ -2,16 +2,23 @@
 //  Definitions
 
 const int led_Pin = 8;
+const int audio_Pin = 5;
 
 ///////////////////////////////
 //  Code
 
 void setup() {
   // put your setup code here, to run once:
-  
+  Serial.begin(9600);
+  pinMode(led_Pin, OUTPUT);
+  pinMode(audio_Pin, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  bool sensorValue = digitalRead(audio_Pin);
+  digitalWrite(led_Pin, sensorValue);
+  if(sensorValue){
+    delay(300); 
+  }
 }
